381a579ba3a9830d2a6bb247372557384cd90e81
tnawaz-git <tnawaz2009@gmail.com>
2023-01-25 01:41:58 +0500

    Invoices, Merchant Account, Add Cash to Account,Transfer Money, Request/Edit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,98 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 const bodyParser = require("body-parser")
 const cors = require("cors")
 
+const transferMoney = async (senderId, receiverId, amount) => {
+  // Find the sender and receiver in the database
+  const sender = await User.findById(senderId);
+  const receiver = await User.findById(receiverId);
+
+  // Check if the sender has enough balance
+  if (sender.balance < amount) {
+    throw new Error("Sender has insufficient balance.");
+  }
+
+  // Create a new transfer from the sender to the receiver
+  const transfer = await stripe.transfers.create({
+    amount: amount * 100, // amount in cents
+    currency: "usd",
+    destination: receiver.stripeAccountId,
+    description: `Transfer to ${receiver.email}`,
+  });
+
+  // Update the sender's and receiver's account balance in the database
+  sender.balance -= amount;
+  receiver.balance += amount;
+  await sender.save();
+  await receiver.save();
+
+  return transfer;
+}
+
+
+const addCashToStripeAccount = async (userId, amount) => {
+  // Find the user in the database
+  const user = await User.findById(userId);
+
+  // Create a new charge on the user's Stripe account
+  const charge = await stripe.charges.create({
+    amount: amount * 100, // amount in cents
+    currency: "usd",
+    customer: user.stripeCustomerId,
+    description: "Adding cash to account",
+  });
+
+  // Update the user's account balance in the database
+  user.balance += charge.amount / 100;
+  await user.save();
+
+  return charge;
+}
+
+
+async function createMerchantAccount(merchantData) {
+  try {
+    const account = await stripe.accounts.create({
+      type: "custom",
+      business_type: "individual",
+      email: merchantData.email,
+      individual: {
+        first_name: merchantData.firstName,
+        last_name: merchantData.lastName,
+        id_number: merchantData.idNumber,
+        ssn_last_4: merchantData.ssnLast4,
+        address: {
+          line1: merchantData.addressLine1,
+          line2: merchantData.addressLine2,
+          city: merchantData.city,
+          state: merchantData.state,
+          postal_code: merchantData.postalCode,
+          country: merchantData.country,
+        },
+        dob: {
+          day: merchantData.dobDay,
+          month: merchantData.dobMonth,
+          year: merchantData.dobYear,
+        },
+        verification: {
+          document: {
+            front: merchantData.verificationDocumentFront,
+            back: merchantData.verificationDocumentBack,
+          },
+        },
+      },
+      tos_acceptance: {
+        date: Math.floor(Date.now() / 1000),
+        ip: merchantData.ip,
+      },
+    });
+    return account;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StripeProvider, Elements, Invoice } from "react-stripe-elements";
+import { useState } from "react";
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +18,120 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+class PaymentApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customer: null,
+      invoice: null,
+    };
+  }
+
+ TransferMoney = ({ userId }) => {
+  const [receiverEmail, setReceiverEmail] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);}
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setSuccess(false);
+    try {
+      // Find the receiver in the database
+      const receiver = await User.findOne({ email: receiverEmail });
+      if (!receiver) {
+        throw new Error("Receiver email not found.");
+      }
+      const transfer = await transferMoney(userId, receiver._id, amount);
+      setSuccess(true);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  AddCash = ({ userId }) => {
+    const [amount, setAmount] = useState(0);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError(null);
+      setSuccess(false);
+      try {
+        const charge = await addCashToStripeAccount(userId, amount);
+        setSuccess(true);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Amount:
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </label>
+        <button type="submit">Add Cash</button>
+        {error && <p>{error}</p>}
+        {success && <p>Cash added successfully!</p>}
+      </form>
+    );
+  };
+
+  // Create a new customer with Stripe
+  createCustomer = async (token) => {
+    const response = await fetch("/create-customer", {
+      method: "POST",
+      body: JSON.stringify({ token }),
+    });
+    const customer = await response.json();
+    this.setState({ customer });
+  };
+
+  // Create an invoice with Stripe
+  createInvoice = async (amount) => {
+    const { customer } = this.state;
+    const response = await fetch("/create-invoice", {
+      method: "POST",
+      body: JSON.stringify({ customer, amount }),
+    });
+    const invoice = await response.json();
+    this.setState({ invoice });
+  };
+
+  // Send the invoice to the customer
+  sendInvoice = async () => {
+    const { invoice } = this.state;
+    await fetch("/send-invoice", {
+      method: "POST",
+      body: JSON.stringify({ invoice }),
+    });
+  };
+
+  render() {
+    return (
+      <StripeProvider apiKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}>
+        <Elements>
+          <div className="checkout-form">
+            <CardForm
+              createCustomer={this.createCustomer}
+              createInvoice={this.createInvoice}
+            />
+            {this.state.invoice && (
+              <button onClick={this.sendInvoice}>Send Invoice</button>
+            )}
+          </div>
+        </Elements>
+      </StripeProvider>
+    );
+  }
+}
+
+
